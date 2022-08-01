8e88817eb30f037e7902e29c0420930168ee5744
Matthias Hertel <hertelm@informatik.uni-freiburg.de>
2022-08-01 11:33:18 +0200

    add arguments -f and -o

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 
+import torch
 import yaml
 import time
 from tqdm import tqdm
@@ -15,8 +16,10 @@ def read_config(config_file: str):
 
 def main(args):
     config = read_config(args.config_file)
-    print(config)
+    #print(config)
+    #print("CUDA available:", torch.cuda.is_available())
 
+    print("load model...")
     spell_checker = GPT2SpellChecker(model=config["model"],
                                      tokenizer=config["tokenizer"],
                                      n_words=config["n_words"],
@@ -33,18 +36,21 @@ def main(args):
                                      prune_beams=config["prune_beams"],
                                      pruning_delta=config["pruning_delta"])
 
-    if config["input_file"] == "None":
+    if config["input_file"] == "None" and not args.f:
         while True:
             query = input("> ")
             result = spell_checker.correct(query, verbose=config["verbose"])
-            print(result)
+            if not config["verbose"] and not config["out_file"]:
+                print(result)
 
     else:
-        with open(config["input_file"]) as in_file:
+        input_file = args.f if args.f else config["input_file"]
+        output_file = args.o if args.o else config["output_file"]
+        with open(input_file) as in_file:
             sequences = in_file.read().splitlines()
         sequences = sequences[args.start:(args.end if args.end is None else args.end + 1)]
-        if config["output_file"] != "None":
-            out_file_name = config["output_file"]
+        if output_file != "None":
+            out_file_name = output_file
             if args.start is not None:
                 out_file_name += f".{args.start}-{args.end}"
             out_file = open(out_file_name, "w")
@@ -56,17 +62,21 @@ def main(args):
             if is_upper:
                 sequence = sequence.lower()
             start_time = time.time()
-            result = spell_checker.correct(sequence, verbose=config["verbose"])
+            try:
+                result = spell_checker.correct(sequence, verbose=config["verbose"])
+            except RuntimeError:
+                result = sequence
+                print("WARNING! RuntimeError for sequence:", sequence)
             runtime += time.time() - start_time
             if is_upper:
                 result = result.upper()
             if out_file is not None:
                 out_file.write(result)
                 out_file.write("\n")
-        if out_file is not None:
-            out_file.write(str(runtime))
-            out_file.write("\n")
-            out_file.close()
+        #if out_file is not None:
+        #    out_file.write(str(runtime))
+        #    out_file.write("\n")
+        #    out_file.close()
 
 
 if __name__ == "__main__":
@@ -74,5 +84,7 @@ if __name__ == "__main__":
     parser.add_argument("config_file", type=str, default="config.yml", nargs="?")
     parser.add_argument("--start", type=int, default=None, required=False)
     parser.add_argument("--end", type=int, default=None, required=False)
+    parser.add_argument("-f", type=str, required=False)
+    parser.add_argument("-o", type=str, required=False)
     args = parser.parse_args()
     main(args)
